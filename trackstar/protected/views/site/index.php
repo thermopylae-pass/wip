7192b6ed2f089b1adcf8bf91c21269df82f65a7c
Andrew Richard Martinez <andrew_martinez@hotmail.com>
2012-11-13 10:49:51 -0700

    show last logged in date/time

--- trackstar/protected/views/site/index.php
+++ trackstar/protected/views/site/index.php
@@ -5,7 +5,11 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
+<?php if ( !Yii::app()->user->isGuest): ?>
+<p>
+    You last logged in on <?php echo date('l, F d, Y, g:i 1', Yii::app()->user->lastLoginTime ); ?>
+</p>
+<?php endif; ?>
 <p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
